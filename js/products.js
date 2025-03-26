14bebbfa39ffc20d393de86105da00820c980765
Christian Reed <reed@Christians-MacBook-Pro.local>
2025-03-26 11:04:52 +0100

    updated css

--- js/products.js
+++ js/products.js
@@ -1,46 +1,5 @@
 let mainContent = document.getElementById('main');
 
-/* document.addEventListener('DOMContentLoaded', function (event) {
-	fetch('/JSON/products.json')
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (myJson) {
-			//console.log(myJson);
-
-			myJson.categories.forEach(function (category) {
-                mainContent.innerHTML += `<article class="category-products">`;
-                
-				mainContent.innerHTML += `<h2>${category.name}</h2>`;
-
-
-				category.products.forEach(function (product) {
-					addProduct(
-						product.title,
-						product.image,
-						product.description,
-						product.price
-					);
-				});
-
-				mainContent.innerHTML += `</article>`;
-			});
-		});
-});
-
-function addProduct(title, image, description, price) {
-	const productHTML = `
-    <div class="product">
-        <h2>${title}</h2>
-        <img src="${image}" alt="${title}">
-        <p>${description}</p>
-        <p>${(price * 1.25).toLocaleString()} kr</p>
-    </div>
-    `;
-
-	mainContent.innerHTML += productHTML;
-}
-*/
 document.addEventListener('DOMContentLoaded', function (event) {
 	// Check if the URL has a 'product' query parameter; if yes, skip rendering the list
 	const urlParams = new URLSearchParams(window.location.search);
@@ -66,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
 				category.products.forEach(function (product) {
 					productHTML += `
-                        <div class="product">
+                        <div class="products">
                             <a href="storepage.html?product=${product.id}">
                                 <img src="${product.image}" alt="${
 						product.title
